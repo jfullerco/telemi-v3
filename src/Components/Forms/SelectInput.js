822690c743f7710fe0c9046b5cf2e0e025072ba9
jfullerco <jonathan@jfuller.co>
2021-04-28 13:38:18 -0400

    1 file added

---
+++ src/Components/Forms/SelectInput.js
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react'
+
+const SelectInput = ({inputFieldName, inputFieldRef, inputFieldValue, inputFieldLabel, inputFieldChange}) => {
+
+  const handleChange = () => {
+   inputFieldChange()
+  }
+
+  return(
+      <div className="field">
+      <label className="label">{inputFieldLabel}</label>
+        <div className="control">
+          <input className="input is-rounded" type="text" ref={inputFieldRef} defaultValue={inputFieldValue} onChange={handleChange} />
+        </div>
+      </div>
+  )
+}
+export default SelectInput
