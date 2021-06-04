5ce96c84cf6b4cf4f7b7076c3210bb370f7c5664
jfullerco <jonathan@jfuller.co>
2021-06-04 13:21:50 -0400

    1 file modified

--- src/Components/Buttons/DeleteButton.js
+++ src/Components/Buttons/DeleteButton.js
@@ -1,17 +1,32 @@
 import React, {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 
 import {db} from '../../Contexts/firebase'
 import ConfirmationModal from '../ConfirmationModal'
 
 const DeleteButton = ({colRef, docRef}) => {
 
+  const history = useHistory()
+
   const [toggleConfirmation, setToggleConfirmation] = useState(false)
   const [confirmed, setConfirmed ] = useState(false)
+  const [deleteSuccess, setDeleteSuccess] = useState()
+  const [deleteError, setDeleteError] = useState()
 
   const handleClick = async() => {
-    
+    try {
     const res = await db.collection(colRef).doc(docRef).delete()
+    setDeleteSuccess("Successfully Deleted Record")
     setToggleConfirmation(!toggleConfirmation)
+    autoClose()
+     } catch {
+      setDeleteError("Error Deleting Record")
+    }
+    userContext.setDataLoading(true)
+  }
+
+  const autoClose = () => {
+    setTimeout(() => {history.push("/dashboard")}, 1500)
   }
 
   const handleConfirmation = () => {
@@ -25,6 +40,8 @@ const DeleteButton = ({colRef, docRef}) => {
       <button className="button is-small is-danger is-rounded ml-2" onClick={()=> {handleConfirmation()}}>Delete</button>
       {toggleConfirmation != false ? 
       <ConfirmationModal header="Confirm Delete">
+        {deleteSuccess && <div className="notification is-success">{deleteSuccess}</div>}
+        {deleteError && <div className="notification is-danger">{deleteError}</div>}
         <button className="button is-rounded is-danger" onClick={() => {handleClick()}}>Delete</button> <button className="button is-rounded" onClick={()=> {handleConfirmation()}}>Cancel</button>
       </ConfirmationModal>
       : ""}
