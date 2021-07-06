2b86547883039bed04d79a6e56ced4dbf5f90d4d
jfullerco <jonathan@jfuller.co>
2021-07-06 17:44:30 -0400

    1 file modified

--- src/Components/Layout/FieldHover.js
+++ src/Components/Layout/FieldHover.js
@@ -4,7 +4,12 @@ const FieldHover = ({isVisible, children}) => {
   return(
     <div className={isVisible === true ? "dropdown is-up is-active" : "is-hidden"}>
       <div className="dropdown-menu">
-      <div className="dropdown-content"><div className="dropdown-item">{children}</div></div></div>
+        <div className="dropdown-content">
+          <div className="dropdown-item">
+            {children}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
